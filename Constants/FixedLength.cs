fc9921026f196553c28154afa5afa28852b7f251
Hussein Beygi <hussein1997.hb@hotmail.com>
2023-02-15 20:21:21 +0330

    Added Shared Projects

---
+++ Constants/FixedLength.cs
@@ -0,0 +1,16 @@
+﻿namespace Constants;
+
+public static class FixedLength : object
+{
+	static FixedLength()
+	{
+	}
+
+	public const int NationalCode = 10;
+	public const int Mobile = 14;
+	public const int Address = 255;
+	public const int Sheba = 30;
+	public const int AccountNumber = 10;
+	public const int DatabasePassword = 44;
+	public const int CardNumber = 16;
+}
---
+++ Constants/MaxLength.cs
@@ -0,0 +1,28 @@
+﻿namespace Constants;
+
+public static class MaxLength : object
+{
+	static MaxLength()
+	{
+	}
+
+	public const int IP = 15;
+
+	public const int Name = 50;
+	public const int Title = 50;
+
+	public const int Link = 500;
+
+	public const int Icon = 100;
+
+	public const int Username = 20;
+	public const int FullName = 50;
+	public const int Password = 20;
+
+	public const int EmailAddress = 254;
+
+	public const int Description = 254;
+
+	public const int CellPhoneNumberVerificationKey = 10;
+
+}
